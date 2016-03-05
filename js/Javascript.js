72c89b9c9099f987b5dd8dc962d5c6745b0a61f9
Kickstv <kicksmedia1@gmail.com>
2016-03-05 18:05:36 +0000

    ..

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,66 +1,116 @@
-var Paintings = function() {
-	var painting_name = document.getElementById("painting-name");
-	var painting_info = document.getElementById("painting-info");
-	$('.image1').click(function() {
-		$('.image-hide').show(100).attr('id', 'image1');
-		painting_name.innerHTML = "Painting 1";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 1";
-	});
-	$('.image2').click(function() {
-		$('.image-hide').show(100).attr('id', 'image2');
-		painting_name.innerHTML = "Painting 2";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 2";
-	});
-	$('.image3').click(function() {
-		$('.image-hide').show(100).attr('id', 'image3');
-		painting_name.innerHTML = "Painting 3";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 3";
-	});
-	$('.image4').click(function() {
-		$('.image-hide').show(100).attr('id', 'image4');
-		painting_name.innerHTML = "Painting 4";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 4";
-	});
-	$('.image5').click(function() {
-		$('.image-hide').show(100).attr('id', 'image5');
-		painting_name.innerHTML = "Painting 5";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 5";
-	});
-	$('.image6').click(function() {
-		$('.image-hide').show(100).attr('id', 'image6');
-		painting_name.innerHTML = "Painting 6";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 6";
-	});
-	$('.image7').click(function() {
-		$('.image-hide').show(100).attr('id', 'image7');
-		painting_name.innerHTML = "Painting 7";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 7";
-	});
-	$('.image8').click(function() {
-		$('.image-hide').show(100).attr('id', 'image8');
-		painting_name.innerHTML = "Painting 8";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 8";
-	});
-	$('.image9').click(function() {
-		$('.image-hide').show(100).attr('id', 'image9');
-		painting_name.innerHTML = "Painting 9";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 9";
-	});
-	$('.image10').click(function() {
-		$('.image-hide').show(100).attr('id', 'image10');
-		painting_name.innerHTML = "Painting 10";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 10";
-	});
-	$('.image11').click(function() {
-		$('.image-hide').show(100).attr('id', 'image11');
-		painting_name.innerHTML = "Painting 11";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 11";
-	});
-	$('.image12').click(function() {
-		$('.image-hide').show(100).attr('id', 'image12');
-		painting_name.innerHTML = "Painting 12";
-		painting_info.innerHTML = "Here goes some information about the painting for painting 12";
-	});
-};
+var step = 1;
 
-$(document).ready(Paintings);
+
+function changeImage(event) {
+	event = event || window.event;
+
+	var targetElement = event.target || event.srcElement;
+	var paintingName = document.getElementById("painting-name");
+	var currentSrc = targetElement.getAttribute("src");
+	var mainImage = document.getElementById("mainImage");
+
+
+	var image1 = document.getElementById("image1");
+	image1.src = "/img/thumbnails/image1.jpg";
+	image1 = image1.src
+
+	var image2 = document.getElementById("image2");
+	image2.src = "/img/thumbnails/image2.jpg";
+	image2 = image2.src
+
+	var image3 = document.getElementById("image3");
+	image3.src = "/img/thumbnails/image3.jpg";
+	image3 = image3.src
+
+	var image4 = document.getElementById("image4");
+	image4.src = "/img/thumbnails/image4.jpg";
+	image4 = image4.src
+
+	var image5 = document.getElementById("image5");
+	image5.src = "/img/thumbnails/image5.jpg";
+	image5 = image5.src
+
+	var image6 = document.getElementById("image6");
+	image6.src = "/img/thumbnails/image6.jpg";
+	image6 = image6.src
+
+	var image7 = document.getElementById("image7");
+	image7.src = "/img/thumbnails/image7.jpg";
+	image7 = image7.src
+
+	var image8 = document.getElementById("image8");
+	image8.src = "/img/thumbnails/image8.jpg";
+	image8 = image8.src
+
+	var image9 = document.getElementById("image9");
+	image9.src = "/img/thumbnails/image9.jpg";
+	image9 = image9.src
+
+	var image10 = document.getElementById("image10");
+	image10.src = "/img/thumbnails/image10.jpg";
+	image10 = image10.src
+
+	var image11 = document.getElementById("image11");
+	image11.src = "/img/thumbnails/image11.jpg";
+	image11 = image11.src
+
+	var image12 = document.getElementById("image12");
+	image12.src = "/img/thumbnails/image12.jpg";
+	image12 = image12.src
+
+
+	if (targetElement.tagName == "IMG" && targetElement.className !== "arrow-left" && targetElement.className !== "arrow-right") {
+		mainImage.src = targetElement.getAttribute("src");
+
+	
+
+		if (currentSrc == "/img/thumbnails/image1.jpg") {
+		paintingName.innerHTML = "Painting1";
+		}
+		else if (currentSrc == "/img/thumbnails/image2.jpg") {
+			paintingName.innerHTML = "Painting2"
+		}
+		else if (currentSrc == "/img/thumbnails/image3.jpg") {
+			paintingName.innerHTML = "Painting3"
+		}
+		else if (currentSrc == "/img/thumbnails/image4.jpg") {
+			paintingName.innerHTML = "Painting4"
+		}
+		else if (currentSrc == "/img/thumbnails/image5.jpg") {
+			paintingName.innerHTML = "Painting5"
+		}
+		else if (currentSrc == "/img/thumbnails/image6.jpg") {
+			paintingName.innerHTML = "Painting6"
+		}
+		else if (currentSrc == "/img/thumbnails/image7.jpg") {
+			paintingName.innerHTML = "Painting7"
+		}
+		else if (currentSrc == "/img/thumbnails/image8.jpg") {
+			paintingName.innerHTML = "Painting8"
+		}
+		else if (currentSrc == "/img/thumbnails/image9.jpg") {
+			paintingName.innerHTML = "Painting9"
+		}
+		else if (currentSrc == "/img/thumbnails/image10.jpg") {
+			paintingName.innerHTML = "Painting10"
+		}
+		else if (currentSrc == "/img/thumbnails/image11.jpg") {
+			paintingName.innerHTML = "Painting11"
+		}
+		else if (currentSrc == "/img/thumbnails/image12.jpg") {
+			paintingName.innerHTML = "Painting12"
+		}
+
+	}
+
+
+
+	else if (targetElement.className == "arrow-right") {
+		step++
+		mainImage.src = eval("image" + step);
+	}
+	else if (targetElement.className == "arrow-left") {
+		step--
+		mainImage.src = eval("image" + step);
+	}
+}
