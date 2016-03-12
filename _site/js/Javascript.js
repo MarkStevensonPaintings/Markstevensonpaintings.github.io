05c8c0c2033a7bc727f29872c0317222c03ce78f
Kickstv <kicksmedia1@gmail.com>
2016-03-12 20:28:22 +0000

    changed gallery view

--- _site/js/Javascript.js
+++ _site/js/Javascript.js
@@ -3,7 +3,6 @@ var step = 1;
 
 function changeImage(event) {
 	event = event || window.event;
-
 	var targetElement = event.target || event.srcElement;
 	var paintingName = document.getElementById("painting-name");
 	var currentSrc = targetElement.getAttribute("src");
@@ -44,7 +43,6 @@ function changeImage(event) {
 	var image12 = document.getElementById("image12");
 	image12.src = "/img/paintings/image12.jpg";
 	image12 = image12.src
-
 	var text1 = "Painting1";
 	var text2 = "Painting2";
 	var text3 = "Painting3";
@@ -59,58 +57,71 @@ function changeImage(event) {
 	var text12 = "Painting12";
 
 
-
+	//  Checks what is being clicked on
 	if (targetElement.tagName == "IMG" && targetElement.className !== "arrow-left" && targetElement.className !== "arrow-right" && targetElement.className !== "cross") {
-		$('.footer-hide').hide();
-		$('.container-hide').show(1000);
-		$('.container-hide2').hide();
+		$('.container-hide').show(300);
 		mainImage.src = targetElement.getAttribute("src");
+		document.getElementById("container2").style.opacity = "0.07";
+		document.getElementsByTagName("body")[0].style.backgroundColor = "#404040";
+		document.getElementById("mynavbar").style.opacity = "0.01";
 
 	
-
+		//  Title for the painting
 		if (currentSrc == "/img/paintings/image1.jpg") {
-		paintingName.innerHTML = "Painting1";
+			paintingName.innerHTML = "Painting1";
+			step = 1
 		}
 		else if (currentSrc == "/img/paintings/image2.jpg") {
 			paintingName.innerHTML = "Painting2"
+			step = 2
 		}
 		else if (currentSrc == "/img/paintings/image3.jpg") {
 			paintingName.innerHTML = "Painting3"
+			step = 3
 		}
 		else if (currentSrc == "/img/paintings/image4.jpg") {
 			paintingName.innerHTML = "Painting4"
+			step = 4
 		}
 		else if (currentSrc == "/img/paintings/image5.jpg") {
 			paintingName.innerHTML = "Painting5"
+			step = 5
 		}
 		else if (currentSrc == "/img/paintings/image6.jpg") {
 			paintingName.innerHTML = "Painting6"
+			step = 6
 		}
 		else if (currentSrc == "/img/paintings/image7.jpg") {
 			paintingName.innerHTML = "Painting7"
+			step = 7
 		}
 		else if (currentSrc == "/img/paintings/image8.jpg") {
 			paintingName.innerHTML = "Painting8"
+			step = 8
 		}
 		else if (currentSrc == "/img/paintings/image9.jpg") {
 			paintingName.innerHTML = "Painting9"
+			step = 9
 		}
 		else if (currentSrc == "/img/paintings/image10.jpg") {
 			paintingName.innerHTML = "Painting10"
+			step = 10
 		}
 		else if (currentSrc == "/img/paintings/image11.jpg") {
 			paintingName.innerHTML = "Painting11"
+			step = 11
 		}
 		else if (currentSrc == "/img/paintings/image12.jpg") {
 			paintingName.innerHTML = "Painting12"
+			step = 12
 		}
 
 	}
 
-
+	// Changes the to the next image
 	else if (targetElement.className == "arrow-right") {
 		if (step >= 12) {
-			step--
+			step = 0
 		}
 		
 		step++
@@ -118,17 +129,21 @@ function changeImage(event) {
 		paintingName.innerHTML = eval("text" + step);
 
 	}
+	// shows the previos image
 	else if (targetElement.className == "arrow-left") {
 		if (step <= 1) {
-			step++
+			step = 13
 		}
 
 		step--
 		mainImage.src = eval("image" + step);
 		paintingName.innerHTML = eval("text" + step);
 	}
-	else if (targetElement.className == "cross") {
+	// To close the full image preview
+	else {
 		$('.container-hide').hide();
-		$('.container-hide2').show(1000);
+		document.getElementById("container2").style.opacity = "1";
+		document.getElementsByTagName("body")[0].style.backgroundColor = "transparent";
+		document.getElementById("mynavbar").style.opacity = "1";
 	}
 }
