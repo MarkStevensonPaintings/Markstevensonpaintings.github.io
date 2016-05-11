39ba356aa6dc10c00bf77047ed1c928fd05d22d5
KicksTV <Kicksmedia1@gmail.com>
2016-05-11 19:17:33 +0100

    Added last gallery

--- _site/js/Javascript.js
+++ _site/js/Javascript.js
@@ -335,3 +335,142 @@ function changeImage2(event) {
 		document.getElementById("footer-hide").style.opacity = "1";
 	}
 }
+
+function changeImage3(event) {
+	event = event || window.event;
+	var targetElement = event.target || event.srcElement;
+	var paintingName = document.getElementById("painting-name");
+	var currentSrc = targetElement.getAttribute("src");
+	var mainImage = document.getElementById("mainImage");
+	
+	
+	var image1 = document.getElementById("image20");
+	image1.src = "/img/paintings/image18P.png";
+	image1 = image1.src
+	var image2 = document.getElementById("image21");
+	image2.src = "/img/paintings/image19P.png";
+	image2 = image2.src
+	var image3 = document.getElementById("image22");
+	image3.src = "/img/paintings/image20P.png";
+	image3 = image3.src
+	var image4 = document.getElementById("image23");
+	image4.src = "/img/paintings/image21L.png";
+	image4 = image4.src
+	var image5 = document.getElementById("image24");
+	image5.src = "/img/paintings/image22L.png";
+	image5 = image5.src
+	var image6 = document.getElementById("image25");
+	image6.src = "/img/paintings/image23L.png";
+	image6 = image6.src
+	var image7 = document.getElementById("image26");
+	image7.src = "/img/paintings/image24L.png";
+	image7 = image7.src
+
+	var text1 = "Painting1";
+	var text2 = "Painting2";
+	var text3 = "Painting3";
+	var text4 = "Painting4";
+	var text5 = "Painting5";
+	var text6 = "Painting6";
+	var text7 = "Painting7";
+
+	//  Checks what is being clicked on
+	if (targetElement.tagName == "IMG" && targetElement.className !== "arrow-left" && targetElement.className !== "arrow-right") {
+		$('.container-hide').show(300);
+		mainImage.src = targetElement.getAttribute("src");
+		document.getElementById("container2").style.opacity = "0.07";
+		document.getElementsByTagName("body")[0].style.backgroundColor = "#404040";
+		document.getElementById("mynavbar").style.opacity = "0.01";
+		document.getElementById("footer-hide").style.opacity = "0.01";
+
+	
+		//  Title for the painting
+		if (currentSrc == "/img/paintings/image18P.png") {
+			paintingName.innerHTML = "Painting1";
+			step = 1
+		}
+		else if (currentSrc == "/img/paintings/image19P.png") {
+			paintingName.innerHTML = "Painting2"
+			step = 2
+		}
+		else if (currentSrc == "/img/paintings/image20P.png") {
+			paintingName.innerHTML = "Painting3"
+			step = 3
+		}
+		else if (currentSrc == "/img/paintings/image21L.png") {
+			paintingName.innerHTML = "Painting4"
+			step = 4
+		}
+		else if (currentSrc == "/img/paintings/image22L.png") {
+			paintingName.innerHTML = "Painting5"
+			step = 5
+		}
+		else if (currentSrc == "/img/paintings/image23L.png") {
+			paintingName.innerHTML = "Painting6"
+			step = 6
+		}
+		else if (currentSrc == "/img/paintings/image24L.png") {
+			paintingName.innerHTML = "Painting7"
+			step = 7
+		}
+
+		if (currentSrc.includes("L")) {
+			mainImage.height = 1080
+			mainImage.width = 1920
+		}
+		else if (currentSrc.includes("P")) {
+			mainImage.height = 700
+			mainImage.width = 600
+		}
+	}
+
+	// Changes the to the next image
+	else if (targetElement.className == "arrow-right") {
+		if (step >= 7) {
+			step = 0
+		}
+		step++
+
+		if (step == 4 || step == 5 || step == 6 || step == 7) {
+			mainImage.height = 1080
+			mainImage.width = 1920
+		}
+		if (step == 1 || step == 2 || step == 3) {
+			mainImage.height = 700
+			mainImage.width = 600
+		}
+		
+		
+		mainImage.src = eval("image" + step);
+		paintingName.innerHTML = eval("text" + step);
+
+	}
+	// shows the previos image
+	else if (targetElement.className == "arrow-left") {
+		if (step <= 1) {
+			step = 8
+		}
+		step--
+
+		if (step == 4 || step == 5 || step == 6 || step == 7) {
+			mainImage.height = 1080
+			mainImage.width = 1920
+		}
+		if (step == 1 || step == 2 || step == 3) {
+			mainImage.height = 700
+			mainImage.width = 600
+		}
+
+		
+		mainImage.src = eval("image" + step);
+		paintingName.innerHTML = eval("text" + step);
+	}
+	// To close the full image preview
+	else {
+		$('.container-hide').hide();
+		document.getElementById("container2").style.opacity = "1";
+		document.getElementsByTagName("body")[0].style.backgroundColor = "transparent";
+		document.getElementById("mynavbar").style.opacity = "1";
+		document.getElementById("footer-hide").style.opacity = "1";
+	}
+}
